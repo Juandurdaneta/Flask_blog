0f163db8dff4a31e1f8473d3dfeecc9ce018a6b7
Juan Urdaneta <juandurdanet@gmail.com>
2021-07-17 20:17:49 -0400

    Testing

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -34,5 +34,8 @@ def create_app(config_class=Config):
     app.register_blueprint(main)
     app.register_blueprint(errors)
 
+    with app.app_context():
+        db.create_all()
+
 
     return app
